a31b16c42b473fff5954f3440970756240e9098e
Nikolay Gorohovsky <fi1a@icloud.com>
2023-02-13 05:09:03 +0500

    Сокращено название css классов

--- resources/html-handler.js
+++ resources/html-handler.js
@@ -1,46 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     function toggle() {
         let container = this.nextSibling;
-        while (!container.classList.contains('var-dumper-container')) {
+        while (!container.classList.contains('vd-container')) {
             container = container.nextSibling;
         }
 
         let dots = this;
-        while (!dots.classList.contains('var-dumper-dots')) {
+        while (!dots.classList.contains('vd-dots')) {
             dots = dots.previousSibling;
         }
 
         let node = this;
-        while (!node.classList.contains('var-dumper-node')) {
+        while (!node.classList.contains('vd-node')) {
             node = node.nextSibling;
         }
 
-        if (this.classList.contains('var-dumper-expanded')) {
-            node.classList.remove('var-dumper-expanded');
-            node.classList.add('var-dumper-collapsed');
+        if (this.classList.contains('vd-expanded')) {
+            node.classList.remove('vd-expanded');
+            node.classList.add('vd-collapsed');
 
-            dots.classList.remove('var-dumper-expanded');
-            dots.classList.add('var-dumper-collapsed');
+            dots.classList.remove('vd-expanded');
+            dots.classList.add('vd-collapsed');
 
-            container.classList.remove('var-dumper-container-open');
-            container.classList.add('var-dumper-container-close');
+            container.classList.remove('vd-container-open');
+            container.classList.add('vd-container-close');
 
             return;
         }
 
-        node.classList.remove('var-dumper-collapsed');
-        node.classList.add('var-dumper-expanded');
+        node.classList.remove('vd-collapsed');
+        node.classList.add('vd-expanded');
 
-        dots.classList.remove('var-dumper-collapsed');
-        dots.classList.add('var-dumper-expanded');
+        dots.classList.remove('vd-collapsed');
+        dots.classList.add('vd-expanded');
 
-        container.classList.remove('var-dumper-container-close');
-        container.classList.add('var-dumper-container-open');
+        container.classList.remove('vd-container-close');
+        container.classList.add('vd-container-open');
     }
-    for (let element of document.getElementsByClassName('var-dumper-dots')) {
+    for (let element of document.getElementsByClassName('vd-dots')) {
         element.onclick = toggle;
     }
-    for (let element of document.getElementsByClassName('var-dumper-node')) {
+    for (let element of document.getElementsByClassName('vd-node')) {
         element.onclick = toggle;
     }
 });
